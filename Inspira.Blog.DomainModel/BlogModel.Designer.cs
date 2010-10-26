2fcd1f0e41566819eb7faf62121146b83dd1e52f
Thiago Alves <thiago.alves@inspira.com.br>
2010-10-26 16:35:20 -0200

    Added copyright notice in *.cs files and license info in lgpl.txt

--- Inspira.Blog.DomainModel/BlogModel.Designer.cs
+++ Inspira.Blog.DomainModel/BlogModel.Designer.cs
@@ -1,3 +1,25 @@
+﻿/*
+Copyright 2010 Inspira Tecnologia.
+All Rights Reserved.
+
+Contact: Thiago Alves <thiago.alves@inspira.com.br>
+
+This file is part of Web.Generics
+
+Web.Generics is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Web.Generics is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Web.Generics.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 ﻿// Default code generation is disabled for model 'C:\Users\alexandre.carvalho\Documents\Visual Studio 2010\Projects\Web.Genericsv2.0\Inspira.Blog.DomainModel\BlogModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
